2323c2da2bd1bbb4bdc75d55098c3cccb8135d94
GePlusE <eisenbeiner@me.com>
2020-12-22 17:59:53 +0100

    added mycreds.json to up- & download

--- Script.py
+++ Script.py
@@ -4,24 +4,31 @@ import time
 import PyDrive_Modul as PYD
 
 
-def main():
-    ############## Settings ##############
-    data_file = "fact_Player2.csv"
-    log_file = ""
-    ######################################
+############## Settings ##############
+data_file = "fact_Player2.csv"
+creds_file = "mycreds.json"
+######################################
 
+
+def main():
+    # starting the whole Scrapping Script
     t0 = time.time()
 
+    # Downloading
+    PYD.download_file(data_file)
+    PYD.download_file(creds_file)
+
+    # Process
     Process = Process_Class.Process()
     club_ranks = Process.club_table_dict
     ID_set = Process.player_ID_set
     Process.sequential_load(ID_set)
-
-    PYD.download_file(data_file)
-
     Process.write_to_csv(Process.dataset, data_file)
 
+    # Uploading
     PYD.update_file(data_file)
+    PYD.update_file(creds_file)
+
     t1 = time.time()
     print(f": Duration: {round(t1 - t0, 2)} seconds.")
 
