73889e5682b67b109099ba50adbeaad9f8922572
GePlusE <eisenbeiner@me.com>
2020-12-30 00:48:44 +0100

    added load_data_to_json

--- Scrape_Transfermarkt.py
+++ Scrape_Transfermarkt.py
@@ -2,6 +2,8 @@ import urllib.request
 from bs4 import BeautifulSoup
 import json
 
+json_file = "player_dim.json"
+
 
 def get_player_IDs(url):
     headers = {
@@ -16,8 +18,6 @@ def get_player_IDs(url):
     result = {}
     result["IDs"] = {}
 
-    # result["IDs"]["ID"]["Key"] = "Value"
-    # print(result)
     for i in players:
         full_name = i.get("title")
         ID = i.get("id")
@@ -62,8 +62,39 @@ def get_transfermarkt_data():
         for key, value in club_dict["IDs"].items():
             dictionary["IDs"][key] = value
 
-    with open("test2.json", "r+", encoding="UTF-8") as f:
-        json.dump(dictionary, f, sort_keys=True, indent=4)
-
     return dictionary
+
+
+def load_data_to_json():
+    tm_data = get_transfermarkt_data()
+
+    with open(json_file, "r+", encoding="UTF-8") as f:
+        data = json.load(f)
+
+    for key, value in tm_data["IDs"].items():
+        last_name = value["Last-Name"]  # Müller
+        first_name = value["First-Name"]  # Thomas
+        full_name = value["Full-Name"]  # Thomas Müller
+        alternative_name = first_name[0] + ". " + last_name  # T. Müller
+        TM_ID = value["Transfermarkt-ID"]
+        TM_url = value["Transfermarkt-URL"]
+        non_matches = set()
+
+        for key, value in data["IDs"].items():
+            name = value["Comunio-Name"]
+            split_name = name.split()[-1]
+            match = [last_name, full_name, alternative_name]
+            if name in match or split_name in match:
+                data["IDs"][key]["Transfermarkt-ID"] = TM_ID
+                data["IDs"][key]["Transfermarkt-URL"] = TM_url
+                data["IDs"][key]["Last-Name"] = last_name
+                data["IDs"][key]["First-Name"] = first_name
+                data["IDs"][key]["Full-Name"] = full_name
+
+    for key, value in data["IDs"].items():
+        if value["Transfermarkt-ID"] == None:
+            non_matches.add(value["Comunio-Name"])
+
+    with open(json_file, "r+", encoding="UTF-8") as f:
+        json.dump(data, f, sort_keys=True, indent=4)
 
