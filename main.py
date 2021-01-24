2c45070a04cc9d31e802c1bdff9cf5275fd48d5d
GePlusE <eisenbeiner@me.com>
2021-01-24 14:20:20 +0100

    preps for deployment

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from Script import main
 main()
 
 # TODO: Before/After every deployment
-# 1. change FolderID back from/to PROD/DEV
+# 1. change FolderID back from/to PROD/DEV in PyDrive_Module
 # 2. change to full PlayerID_set in sequential_load
 # 3. delete prints in sequential_load
 # 4. change json in Player_Class to PROD_file
